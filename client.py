b535ba21fd49cc196be96c2e8dc59dc70a5f925d
AlexeyShulinin <babushka661@gmail.com>
2020-07-19 15:49:47 +0300

    added small fix(code review)

--- client.py
+++ client.py
@@ -2,9 +2,14 @@ import socket
 import time
 import operator
 
+"""
+Class for except errors from custom protocol.
+"""
 class ClientError(Exception):
 	pass
 
+# Client class make connetcion between Clinet and Server.
+# After that Client can send data and get data from Server.
 class Client:
 
 	_Client__connection = 0
@@ -15,7 +20,9 @@ class Client:
 		self.timeout = timeout
 		Client._Client__connection = socket.create_connection((self.host,self.port),timeout=self.timeout)
 
-	#method for getting metrics from server
+	"""
+	Method for getting metrics from server.
+	"""
 	def get(self, metric_name):
 		request = f"get {metric_name}\n"
 		data_recv = ""
@@ -23,36 +30,39 @@ class Client:
 			Client._Client__connection.sendall(request.encode("utf8"))
 			data_recv = Client._Client__connection.recv(1024).decode("utf8")
 
-
 			data_list = data_recv.split('\n')
 
-			if len(data_list[1].split()) != 3 and data_recv != "ok\n\n":
+			if (len(data_list[1].split()) != 3 
+				and data_recv != "ok\n\n"):
+
 				raise ClientError 
+
 		except ClientError as err:
 			print("ClientError: wrong request...\n")
-
 			raise ClientError
 
 		if data_list[0] == 'ok' and len(data_list[1].split()) == 3:
 			return Client.pasre_data_request(data_list)
 		else :
-			return {}		#if request "ok\n\n" - no key
+			return {}  # If request "ok\n\n" - no key.
 
 
-	#method for parse information and make dict of information
+	"""
+	Method for parse information and make dict of information.
+	"""
 	def pasre_data_request(data_list):
-		index = 1
+		_index = 1
 
 		_dict = dict()
-		while index < len(data_list):
-			if(data_list[index] == ''):
+		while _index < len(data_list):
+			if(data_list[_index] == ''):
 				break
-			item = data_list[index].split()
+			item = data_list[_index].split()
 			if item[0] in _dict:
 				_dict[item[0]] += [(int(item[2]),float(item[1]))]
 			else:
 				_dict[item[0]] = [(int(item[2]),float(item[1]))]
-			index += 1
+			_index += 1
 
 		for key,value in _dict.items():
 			_dict[key] = sorted(value) 
@@ -60,7 +70,9 @@ class Client:
 
 
 
-	#method for saving new metrics in server
+	"""
+	Method for saving new metrics in server.
+	"""
 	def put(self, metric_name, metric_value, timestamp = None):
 		if timestamp is None:
 			timestamp = int(time.time()) 
--- server.py
+++ server.py
@@ -1,69 +1,80 @@
 import asyncio
 
-#global server storage
-_storage = dict()
+"""
+Global server storage.
+"""
+storage = dict()
 
-#class wich manipulate with request and data
+"""
+Class wich manipulate with request and data.
+"""
 class Server:
 
 	def __init__(self):
 		pass
 
-	#method save request to storage of server
+	"""
+	Method save request to storage of server.
+	"""
 	def save_request(key, value, timestamp):
-		if key not in _storage:
-			_storage[key] = [(float(value),int(timestamp))]
+		if key not in storage:
+			storage[key] = [(float(value),int(timestamp))]
 		else:
-			values = dict(_storage.get(key))
+			_values = dict(storage.get(key))
 
-			print(values)
+			print(_values)
 
-			values = {v:k for k, v in values.items()}
+			_values = {v:k for k, v in _values.items()}
 
-			print(values)
+			print(_values)
 			_dict = dict()
 
-			#if timestamp is in storage - update data
-			if timestamp in values.keys():
+			if timestamp in _values.keys():  # If timestamp is in storage - update data.
 				print(timestamp,"is chande")
-				values[timestamp] = value
-				for k,v in values.items():
+				_values[timestamp] = value
+				for k,v in _values.items():
 					if k not in _dict:
 						_dict[float(v)] = int(k)
 					else:
 						_dict[float(v)] += int(k)
-				_storage[key] = tuple(_dict.items())
+				storage[key] = tuple(_dict.items())
 			else:
 				_dict[float(value)] = int(timestamp)
-				_storage[key] += tuple(_dict.items())
+				storage[key] += tuple(_dict.items())
 					
 
-	#method get data from storage by correct request
+	"""
+	Method get data from storage by correct request.
+	"""
 	def get_data(request_key):
-		data = ''
-		if request_key in _storage.keys():
-			values = _storage.get(request_key)
+		_data = ''
+
+		if request_key in storage.keys():
+			values = storage.get(request_key)
 			for item in values:
-				data += f'{request_key} {item[0]} {item[1]}\n'
+				_data += f'{request_key} {item[0]} {item[1]}\n'
+
 		if request_key == '*':
-			for key,value in _storage.items():
+			for key,value in storage.items():
 				for item in value:
-					data += f'{key} {item[0]} {item[1]}\n'
-		return f'ok\n{data}\n'
+					_data += f'{key} {item[0]} {item[1]}\n'
 
-	#method for return request to client
+		return f'ok\n{_data}\n'
+
+	"""
+	Method for return request to client.
+	"""
 	def make_request(key):
-		index = 1
-		data = ''
+		_data = ''
 
-		for key,value in _storage.items():
+		for key,value in storage.items():
 			for item in value:
-				data += f'{key} {item[0]} {item[1]}\n'
+				_data += f'{key} {item[0]} {item[1]}\n'
 
 		request = Server.qet_data()
 		return request
 
-	def is_digit(string):
+	def _is_digit(string):
 		if string.isdigit():
 			return True
 		else:
@@ -73,38 +84,54 @@ class Server:
 			except ValueError:
 				return False
 
-
-	#nethod checking request for valid structure 
+	"""
+	Method check request for valid structure.
+	"""
 	def check_request(self,data_recv):
-		recv_list = data_recv.split()
-		print(len(recv_list))
-		if len(recv_list) == 4 or len(recv_list) == 2:
-			if recv_list[0] == 'put' and Server.is_digit(recv_list[3]) and Server.is_digit(recv_list[2]):
-				Server.save_request(recv_list[1], float(recv_list[2]), int(recv_list[3]))
-				print(_storage)
+		_recv_list = data_recv.split()
+
+		if len(_recv_list) == 4 or len(_recv_list) == 2:
+			if (_recv_list[0] == 'put' 
+				and Server._is_digit(_recv_list[3]) 
+				and Server._is_digit(_recv_list[2])):
+
+				Server.save_request(
+					_recv_list[1], 
+					float(_recv_list[2]), 
+					int(_recv_list[3])
+				)
+
 				return 'ok\n\n'
-			elif recv_list[0] == 'get' and len(recv_list) == 2:
-				return Server.get_data(recv_list[1])
+
+			elif _recv_list[0] == 'get' and len(_recv_list) == 2:
+				return Server.get_data(_recv_list[1])
 			else:
 				return 'error\nwrong command\n\n'
 		else:
 			return 'error\nwrong command\n\n'
 
-class ClientServerProtocol(asyncio.Protocol):
 
+# Class which make connection between
+# client and server and make request
+# between them.
+class ClientServerProtocol(asyncio.Protocol):
 
 	def __init__(self):
 		pass
 
-	#make connection between server and client
+	"""
+	Make connection between server and client.
+	"""
 	def connection_made(self, transport):
-		#object of connection
 		self.server = Server()
 		self.transport = transport
 		peername = transport.get_extra_info('peername')
 		print('Connection from {}'.format(peername))
 		
-	#method for receive data between server and client
+
+	"""
+	Method for receive data between server and client.
+	"""
 	def data_received(self, data):
 		resp = data.decode("utf8")
 		print(resp)
@@ -114,6 +141,7 @@ class ClientServerProtocol(asyncio.Protocol):
 
 
 def run_server(host, port):
+
 	loop = asyncio.get_event_loop()
 	coro = loop.create_server(
 		ClientServerProtocol,
@@ -130,6 +158,7 @@ def run_server(host, port):
 	server.close()
 	loop.run_until_complete(server.wait_closed())
 	loop.close()
+
 
 if __name__ == "__main__":
 	run_server("127.0.0.1", 8181)
